7f0ec6b4b51c08af142fade92205283cdf512e9d
patilrohini43 <45816823+patilrohini43@users.noreply.github.com>
2019-12-30 17:29:07 +0530

    Modified:use sub

--- src/com/bridgelabz/demo/Addition.java
+++ src/com/bridgelabz/demo/Addition.java
@@ -7,8 +7,8 @@ public class Addition {
 		System.out.println("Welcome to bridgelabz");
 		int first = 10;
 		int second = 20;
-		int sum = first + second;
-		System.out.println("The sum is: " + sum);
+		int sub = first - second;
+		System.out.println("The sub is: " + sub);
 	}
 
 }
