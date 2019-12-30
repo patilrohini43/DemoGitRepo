acf9fec0b217862f46f548c73fdc62d3747c51d2
rohini <patilrohini43@gmail.com>
2019-12-30 17:23:03 +0530

    Added:logic addition of two numbers

--- src/com/bridgelabz/demo/Addition.java
+++ src/com/bridgelabz/demo/Addition.java
@@ -1,12 +1,14 @@
 package com.bridgelabz.demo;
 
 public class Addition {
-	
 
-    public static void main(String[] args) {
-    
-    	System.out.println("Welcome to bridgelabz");
-    }
-        
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to bridgelabz");
+		int first = 10;
+		int second = 20;
+		int sum = first + second;
+		System.out.println("The sum is: " + sum);
+	}
 
 }
