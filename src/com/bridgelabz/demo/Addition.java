b8f91f39ea8b3d5b886ebc0a7529b76db4bdd20d
rohini <patilrohini43@gmail.com>
2019-12-30 17:18:06 +0530

    Added:Welcome message

---
+++ src/com/bridgelabz/demo/Addition.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.demo;
+
+public class Addition {
+	
+
+    public static void main(String[] args) {
+    
+    	System.out.println("Welcome to bridgelabz");
+    }
+        
+
+}
