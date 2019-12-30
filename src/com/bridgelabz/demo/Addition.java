d13dac912658c2905d0d5f5effb30a007d6f6bda
rohini <patilrohini43@gmail.com>
2019-12-30 17:31:08 +0530

    modified:logic sub of two numbers

--- src/com/bridgelabz/demo/Addition.java
+++ src/com/bridgelabz/demo/Addition.java
@@ -7,8 +7,8 @@ public class Addition {
 		System.out.println("Welcome to bridgelabz");
 		int first = 10;
 		int second = 20;
-		int sum = first + second;
-		System.out.println("The sum is: " + sum);
+		int mul = first * second;
+		System.out.println("The mul is: " + mul);
 	}
 
 }
